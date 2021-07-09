d31d39aa0aad2e7fe1911fcacaa9aa6289830abe
Christian Nuss <christian.nuss@gmail.com>
2021-07-09 11:44:54 -0700

    Formatting

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "name" {
-  type = string
+  type        = string
   description = "The desired account name"
 }
 
 variable "email" {
-  type = string
+  type        = string
   description = "The email for the account (must be unique within Amazon AWS)"
 }
